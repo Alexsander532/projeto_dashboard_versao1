37145f0d6cb69385a5fa8f8aac85dfed694e8d04
seu-nome-de-usuario-github <alexsanderaugusto142019@gmail.com>
2025-02-16 22:36:58 -0300

    Update init.js

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -2,11 +2,11 @@ const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST ,
-  database: process.env.DB_NAME,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  user: 'postgres',
+  host: 'db.nqnlafkiiszhpnzhaugb.supabase.co',
+  database: 'postgres',
+  password: 'Cefet2020.',
+  port: 5432
 });
 
 async function initializeDatabase() {
