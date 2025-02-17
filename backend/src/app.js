21f88cd8fef26fc94bee3eb9b9c1b50230f6efc5
seu-nome-de-usuario-github <alexsanderaugusto142019@gmail.com>
2025-02-16 21:32:02 -0300

    Update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,7 +8,7 @@ const app = express();
 
 // Configuração do CORS
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173', 'http://localhost:5174'],
+  origin: '*',
   credentials: true
 }));
 
