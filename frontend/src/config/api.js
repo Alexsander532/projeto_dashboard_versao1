7e29cd4392e99e966a9a0f0051f571865fec23a7
seu-nome-de-usuario-github <alexsanderaugusto142019@gmail.com>
2025-02-16 23:08:26 -0300

    Update api.js

--- frontend/src/config/api.js
+++ frontend/src/config/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || 'https://projetodashboardversao1-production.up.railway.app/',
+    baseURL:'https://projetodashboardversao1-production.up.railway.app/',
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json'
